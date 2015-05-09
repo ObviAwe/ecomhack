3621bbf1a86fdaed99811d350ce21d814261ae72
Catharina <c.kenklies@satzmedia.de>
2015-05-09 23:39:36 +0200

    struktur

---
+++ wear/src/main/java/com/ecomhack/api/StorageImpl.java
@@ -0,0 +1,21 @@
+package com.ecomhack.api;
+
+import com.ecomhack.product.Product;
+import com.ecomhack.product.Product;
+
+import java.util.List;
+
+/**
+ * Created by ckenklies on 09.05.15.
+ */
+public class StorageImpl implements Storage {
+
+
+    @Override
+    public Product getProduct(List<Product> products) {
+        //TODO: Liste einlesen und Produkte per API holen
+        return null;
+    }
+
+
+}

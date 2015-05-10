b8f17171168aa6b8c20a49c8d2aecada0a584908
Sebastian Pappert <sebastian.pappert@gmail.com>
2015-05-10 12:02:17 +0200

    retrieve token via oauth2

--- wear/src/main/java/com/ecomhack/oa/ProductFinder.groovy
+++ wear/src/main/java/com/ecomhack/oa/ProductFinder.groovy
@@ -8,8 +8,10 @@ import javax.net.ssl.HttpsURLConnection;
 public class ProductFinder {
     def static foodpairingIDs = ['3999', '150', '4948', '4279', '4130']
     def static counter = 0;
+    def static token;
 
     public static void main(String[] args) {
+        token = retrieveToken()
         println getProduct([]).name
         println getProduct([]).name
         println getProduct([]).name
@@ -20,9 +22,9 @@ public class ProductFinder {
     public static Product getProduct(List products) {
             def foodpairingID = foodpairingIDs[counter++]
             String response = "";
-            def url = new URL('https://api.sphere.io/ecomhack-15-002-83/product-projections/search?staged=true&limit=1&filter.query=variants.attributes.foodpairingId:'+foodpairingID)
+            def url = new URL("https://api.sphere.io/ecomhack-15-002-83/product-projections/search?staged=true&limit=1&filter.query=variants.attributes.foodpairingId:$foodpairingID")
             HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection()
-            urlConnection.setRequestProperty('Authorization', 'Bearer 673kjspXv-_JpPcd-S9lw-6oeKjMwiTS')
+            urlConnection.setRequestProperty('Authorization', "Bearer $token")
             BufferedReader input = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
             String inputLine
             while ((inputLine = input.readLine()) != null)
@@ -35,6 +37,23 @@ public class ProductFinder {
             def product = new Product(sphereProduct)
 
             return product
+    }
+
+    public static String retrieveToken() {
+        String response = "";
+
+        def  url = new URL("https://auth.sphere.io/oauth/token?grant_type=client_credentials&scope=manage_project:ecomhack-15-002-83");
+        HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+        urlConnection.setRequestMethod("POST");
+        urlConnection.setRequestProperty("Authorization", "Basic emVQcUJiSXRmMU4tTXBKNng0a1dzOGZ4Ok5FU28teEZoUXFiNXE5N2R3blprMVpfcVVEYVNuMEZG");
+        BufferedReader input = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+        String inputLine;
+        while ((inputLine = input.readLine()) != null)
+            response += inputLine;
+        input.close();
+
+        Map result = new Gson().fromJson(response, Map.class)
+        result.access_token
     }
 
 
